4f085cc2fa265cfa4104b1ab2b2dfdb4eff078a5
Rebel Development <github@radiantdevelopment.net>
2026-03-29 23:21:37 -0400

    Update targeting.lua

--- client/targeting.lua
+++ client/targeting.lua
@@ -10,9 +10,9 @@ local function notify(msg)
     DrawNotification(false, false)
 end
 
--- =============================
--- Builder Toggle
--- =============================
+local function isFirstPerson()
+    return GetFollowPedCamViewMode() == 4
+end
 
 RegisterCommand('incidentbuilder', function()
     BuilderActive = not BuilderActive
@@ -22,15 +22,16 @@ RegisterCommand('incidentbuilder', function()
     else
         notify("Incident Nexus Builder Disabled")
 
-        if HideBuilderPlacedProps then
-            HideBuilderPlacedProps()
+        if IncidentNexusProps then
+            IncidentNexusProps:HideBuilderPlacedProps()
+            IncidentNexusProps:SetActive(false)
+        end
+
+        if IncidentNexusDoors then
+            IncidentNexusDoors:SetActive(false)
         end
     end
 end, false)
-
--- =============================
--- Set Station Name
--- =============================
 
 RegisterCommand('nexussetname', function(_, args)
     local name = table.concat(args, " ")
@@ -46,46 +47,22 @@ RegisterCommand('nexussetname', function(_, args)
     notify(("Station set to %s"):format(BuilderState.stationName))
 end, false)
 
--- =============================
--- Alert Commands
--- =============================
-
 RegisterCommand('nexustest', function()
-
-    TriggerServerEvent(
-        'incident-nexus:server:testAlert',
-        BuilderState.stationId
-    )
-
+    TriggerServerEvent('incident-nexus:server:testAlert', BuilderState.stationId)
 end, false)
 
 RegisterCommand('nexusclear', function()
-
-    TriggerServerEvent(
-        'incident-nexus:server:clearAlert',
-        BuilderState.stationId
-    )
-
+    TriggerServerEvent('incident-nexus:server:clearAlert', BuilderState.stationId)
 end, false)
 
--- =============================
--- Screen Commands
--- =============================
-
 RegisterCommand('nexusscreenidle', function()
-
     if IncidentNexusScreens then
-        IncidentNexusScreens:SetStationIdle(
-            BuilderState.stationId
-        )
+        IncidentNexusScreens:SetStationIdle(BuilderState.stationId)
     end
-
     notify("Screen set to idle")
-
 end, false)
 
 RegisterCommand('nexusscreenalert', function(_, args)
-
     local title = args[1] or "STRUCTURE FIRE"
     local message = table.concat(args, " ", 2)
 
@@ -94,63 +71,31 @@ RegisterCommand('nexusscreenalert', function(_, args)
     end
 
     if IncidentNexusScreens then
-        IncidentNexusScreens:SetStationAlert(
-            BuilderState.stationId,
-            title,
-            message
-        )
+        IncidentNexusScreens:SetStationAlert(BuilderState.stationId, title, message)
     end
-
 end, false)
 
--- =============================
--- Warning Light Commands
--- =============================
-
 RegisterCommand('nexusamber', function()
-
     if IncidentNexusWarningLights then
-        IncidentNexusWarningLights:SetStationMode(
-            BuilderState.stationId,
-            'idle'
-        )
+        IncidentNexusWarningLights:SetStationMode(BuilderState.stationId, 'idle')
     end
-
 end, false)
 
 RegisterCommand('nexusred', function()
-
     if IncidentNexusWarningLights then
-        IncidentNexusWarningLights:SetStationMode(
-            BuilderState.stationId,
-            'alert'
-        )
+        IncidentNexusWarningLights:SetStationMode(BuilderState.stationId, 'alert')
     end
-
 end, false)
 
 RegisterCommand('nexuslightsoff', function()
-
     if IncidentNexusWarningLights then
-        IncidentNexusWarningLights:SetStationMode(
-            BuilderState.stationId,
-            'off'
-        )
+        IncidentNexusWarningLights:SetStationMode(BuilderState.stationId, 'off')
     end
-
 end, false)
 
--- =============================
--- Receive Server Events
--- =============================
-
 RegisterNetEvent('incident-nexus:client:testAlert', function(stationId)
-
     if IncidentNexusWarningLights then
-        IncidentNexusWarningLights:SetStationMode(
-            stationId,
-            'alert'
-        )
+        IncidentNexusWarningLights:SetStationMode(stationId, 'alert')
     end
 
     if IncidentNexusScreens then
@@ -160,83 +105,47 @@ RegisterNetEvent('incident-nexus:client:testAlert', function(stationId)
             "Units respond immediately"
         )
     end
-
 end)
 
 RegisterNetEvent('incident-nexus:client:receiveStations', function(stations)
+    print(("[Incident Nexus] Received %s stations from server"):format(#(stations or {})))
 
-    if LoadStations then
-        LoadStations(stations)
+    if IncidentNexusProps then
+        IncidentNexusProps:LoadStations(stations or {})
     end
-
 end)
 
 RegisterNetEvent('incident-nexus:client:receiveDrafts', function(drafts)
-
-    -- optional debug
-    print("[Incident Nexus] Received drafts:", #drafts)
-
+    print("[Incident Nexus] Received drafts:", #(drafts or {}))
 end)
 
--- =============================
--- Request Data
--- =============================
-
 CreateThread(function()
-
     Wait(1500)
-
-    TriggerServerEvent(
-        'incident-nexus:server:requestStations'
-    )
-
+    TriggerServerEvent('incident-nexus:server:requestStations')
 end)
 
--- =============================
--- First Person Requirement
--- =============================
-
-local function isFirstPerson()
-    return GetFollowPedCamViewMode() == 4
-end
-
--- =============================
--- Builder Loop
--- =============================
-
 CreateThread(function()
-
     while true do
-
         local sleep = 1000
 
         if BuilderActive then
-
             sleep = 0
 
             DisableControlAction(0, 37, true)
-            DisablePlayerFiring(PlayerPedId(), true)
+            DisablePlayerFiring(PlayerId(), true)
 
             if not isFirstPerson() then
-
                 SetTextFont(4)
                 SetTextScale(0.35, 0.35)
                 SetTextColour(255, 0, 0, 255)
                 SetTextCentre(true)
 
                 BeginTextCommandDisplayText("STRING")
-                AddTextComponentSubstringPlayerName(
-                    "You must be in First Person to use builder"
-                )
-
+                AddTextComponentSubstringPlayerName("You must be in First Person to use builder")
                 EndTextCommandDisplayText(0.5, 0.90)
-
             end
-
         end
 
         Wait(sleep)
-
     end
-
 end)
